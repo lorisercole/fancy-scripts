331de4231ae6170d5836f1cf4a10973706cbe915
Riccardo Bertossa <rbertoss@sissa.it>
2018-06-14 14:27:31 +0200

    updated grep_perf to latest version of cp.x (in a back-compatible way)

--- QuantumEspresso/cp.x/grep_perf.sh
+++ QuantumEspresso/cp.x/grep_perf.sh
@@ -16,21 +16,30 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-echo "#MPI_PROCS OMP_THREADS BANDS_GROUPS TASK_GROUPS ORTHO  InitTime 1StepTime  file"
+echo "#MPI_PROCS OMP_THREADS BANDS_GROUPS TASK_GROUPS ORTHO  InitTime 1StepTime  file MAIN_LOOP_CALLS"
 
 for f in $1 
 do
     MPI_PROC=$(grep "Number of MPI"  $f | awk '{print $5}')
+    if [ -z "$MPI_PROCS" ]; then
+      MPI_PROC=$(grep "Parallel version (MPI), running on" $f | awk '{print $6}')
+    fi
     OMP_THREADS=$(grep "Threads/MPI"  $f | awk '{print $3}')
+    if [ -z "$OMP_THREADS" ]; then
+      OMP_THREADS=0
+    fi
     BAND_GROUPS=$(grep "band groups division"  $f | awk '{print $6}')
     if [ -z "$BAND_GROUPS" ]; then
-      BAND_GROUPS=1
+      BAND_GROUPS=0
     fi
     TASK_GROUPS=$(grep "wavefunctions fft division"  $f | awk '{print $8}')
     if [ -z "$TASK_GROUPS" ]; then
-      TASK_GROUPS=1
+      TASK_GROUPS=0
     fi
     ORTHO=$(grep "ortho sub-group" $f | awk '{print $5}')
+    if [ -z "$ORTHO" ]; then
+      ORTHO=0
+    fi
     INIT_TIME=$(grep "initialize" $f | awk '{print $5}')
     MAIN_LOOP=$(grep "main_loop" $f | awk '{print $5}')
     MAIN_LOOP_CALLS=$(grep "main_loop" $f | awk '{print $8}')
@@ -43,6 +52,6 @@ do
       ONE_STEP_TIME=`awk -vtot=${MAIN_LOOP%s} -vcalls=$MAIN_LOOP_CALLS 'BEGIN{printf("%.3f", tot/calls)}'`
     fi
     ff=${f#silica_SCF.70.}
-    echo $MPI_PROC $OMP_THREADS $BAND_GROUPS $TASK_GROUPS $ORTHO  ${INIT_TIME%s} $ONE_STEP_TIME  ${ff%.out}
+    echo $MPI_PROC $OMP_THREADS $BAND_GROUPS $TASK_GROUPS $ORTHO  ${INIT_TIME%s} $ONE_STEP_TIME ${ff%.out} $MAIN_LOOP_CALLS
 done
 
